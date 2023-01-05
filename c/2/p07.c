04bcca4f17bd844ca403db53329b64fa2a805314
marichou10 <nefeli@MARIA-PC.localdomain>
2023-01-05 22:47:58 +0200

    PARTA

---
+++ c/2/p07.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+#include <math.h> 
+
+int main()
+{
+    char b1;
+    char b2;
+    char b3;
+
+    while ( scanf ("%c%c%c", &b1, &b2, &b3) == 3 )
+    {
+        printf("%c%c%c\n", b1, b2, b3 );
+    } 
+}
